aa1aa198180b50d5e199b3ef2221c024026afb0e
David <davidavis2004@gmail.com>
2026-07-21 14:33:28 +0100

    add security and token verification

--- backend/app/schemas/user.py
+++ backend/app/schemas/user.py
@@ -8,6 +8,15 @@ class CreateUser(BaseModel):
     email: EmailStr
     password: str
 
+class UserOut(BaseModel):
+    id: int
+    email: EmailStr
+    created_at: datetime
+    
+    class Config:
+        from_attributes = True
+
+
 class UserLogin(BaseModel):
     email: EmailStr
     password: str
