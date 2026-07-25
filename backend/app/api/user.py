e7c5f13753afa258895eb2e3d3398afd21a2c441
David <davidavis2004@gmail.com>
2026-07-25 20:54:45 +0100

    Added a LogIn api endpoint

--- backend/app/api/user.py
+++ backend/app/api/user.py
@@ -1,15 +1,17 @@
 from fastapi import APIRouter, Depends, FastAPI, status, HTTPException
-from app.schemas.UserSchema import CreateUser, UserOut
+from app.schemas.UserSchema import CreateUser, Token, UserOut
 from app.core.database import get_db
-from app.core.security import hash_password
+from app.core.security import hash_password, verify_password, verify_password, create_access_token
 from app.models import UserModel
+from fastapi.security.oauth2 import OAuth2PasswordRequestForm
+from sqlalchemy.orm import Session
 
 router = APIRouter(
     prefix="/user"
 )
 
 @router.post("/signup", response_model=UserOut, status_code=status.HTTP_201_CREATED)
-def user_signup(user: CreateUser, db: status = Depends(get_db) ):
+def user_signup(user: CreateUser, db: Session = Depends(get_db) ):
     
         
     existing_user = db.query(UserModel.User).filter(UserModel.User.email == user.email).first()
@@ -28,3 +30,18 @@ def user_signup(user: CreateUser, db: status = Depends(get_db) ):
     db.commit()
     db.refresh(new_user)
     return new_user
+
+@router.post("/login", response_model=Token)
+def user_login(user_credentials: OAuth2PasswordRequestForm = Depends(), db: Session = Depends(get_db)):
+    
+    user = db.query(UserModel.User).filter(
+        UserModel.User.email == user_credentials.username).first()
+
+    if not user:
+        raise HTTPException(status_code=status.HTTP_403_FORBIDDEN, detail=f"Invalid Credentials!")
+    
+    if not verify_password(user_credentials.password, user.password):
+        raise HTTPException(status_code=status.HTTP_403_FORBIDDEN, detail=f"Invalid Credentials")
+    
+    access_token = create_access_token(data={"user_id": user.id})
+    return {"access_token": access_token, "token_type": "bearer"}
--- backend/app/core/security.py
+++ backend/app/core/security.py
@@ -53,5 +53,5 @@ pwd_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
 def hash_password(password: str):
     return pwd_context.hash(password)
     
-def verify(password: str, hashed: str):
+def verify_password(password: str, hashed: str):
     return pwd_context.verify(password, hashed)
