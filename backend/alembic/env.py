b4843c63e1800ae6361dc65b9b3c4a22d09f4835
David <davidavis2004@gmail.com>
2026-07-31 17:53:18 +0100

    fix activities table

--- backend/alembic/env.py
+++ backend/alembic/env.py
@@ -22,6 +22,7 @@ if config.config_file_name is not None:
 # from myapp import mymodel
 # target_metadata = mymodel.Base.metadata
 from app.models.UserModel import User
+from app.models.ActivityModel import Activity
 from app.core.database import Base
 target_metadata = Base.metadata
 
---
+++ backend/app/models/ActivityModel.py
@@ -0,0 +1,26 @@
+from app.core.database import Base
+from sqlalchemy import TIMESTAMP, Column, ForeignKey, Integer, String, Boolean, text
+
+class Activity(Base):
+    __tablename__ = "activity"
+
+    activity_id = Column(Integer, primary_key=True, nullable=False)
+    user_id = Column(Integer, ForeignKey("users.id"), nullable=False)
+    date = Column(TIMESTAMP(timezone=True), nullable=False)
+
+    distance_meters = Column(Integer, nullable=False)
+    duration_minutes = Column(Integer, nullable=False)
+    total_elevation_gain = Column(Integer)
+
+    average_pace_seconds = Column(Integer, nullable=False)
+
+    max_heart_rate = Column(Integer, nullable=False)
+    heart_rate_zone_1_minutes = Column(Integer, nullable=False)
+    heart_rate_zone_2_minutes = Column(Integer, nullable=False)
+    heart_rate_zone_3_minutes = Column(Integer, nullable=False)
+    heart_rate_zone_4_minutes = Column(Integer, nullable=False)
+    heart_rate_zone_5_minutes = Column(Integer, nullable=False)
+
+    activity_type = Column(String, nullable=False)
+    perceived_effort = Column(Integer)
+    created_at = Column(TIMESTAMP(timezone=True), nullable=False, server_default=text('now()'))
